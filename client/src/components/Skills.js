2cbd09c4fec7b0c247b227b2c26a72cce0f67147
hamzabelakri <hamzabelakri92@gmail.com>
2023-05-18 22:48:34 +0100

    text-hover

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -4,12 +4,14 @@ import { Icon } from "@iconify/react";
 function Skills() {
   const tools = [
     {
+      name: "Github",
       logo: "bi:github",
       level: "Beginner",
       count: 20,
     },
 
     {
+      name: "Docker",
       logo: "fa-brands:docker",
       level: "Beginner",
       count: 20,
@@ -18,31 +20,37 @@ function Skills() {
 
   const front = [
     {
+      name: "HTML5",
       logo: "ion:logo-html5",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "CSS3",
       logo: "ion:logo-css3",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "React",
       logo: "ion:logo-react",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "Redux",
       logo: "tabler:brand-redux",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "Preact",
       logo: "simple-icons:preact",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "Tailwind",
       logo: "mdi:tailwind",
       level: "Intermediate",
       count: 40,
@@ -51,26 +59,31 @@ function Skills() {
 
   const back = [
     {
+      name: "NodeJs",
       logo: "ion:logo-nodejs",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "Express",
       logo: "simple-icons:express",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "MongoDB",
       logo: "bxl:mongodb",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "NestJs",
       logo: "simple-icons:nestjs",
       level: "Intermediate",
       count: 40,
     },
     {
+      name: "RabbitMQ",
       logo: "simple-icons:rabbitmq",
       level: "Beginner",
       count: 20,
@@ -98,7 +111,10 @@ function Skills() {
                 }}
                 className="w-32 h-32 flex items-center justify-center rounded-full"
               >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
+                <div
+                  className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600"
+                  title={tools.name}
+                >
                   <Icon icon={tools.logo} />
                 </div>
               </div>
@@ -122,7 +138,10 @@ function Skills() {
                 }}
                 className="w-32 h-32 flex items-center justify-center rounded-full"
               >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
+                <div
+                  className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600"
+                  title={front.name}
+                >
                   <Icon icon={front.logo} />
                 </div>
               </div>
@@ -146,7 +165,10 @@ function Skills() {
                 }}
                 className="w-32 h-32 flex items-center justify-center rounded-full"
               >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
+                <div
+                  className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600"
+                  title={back.name}
+                >
                   <Icon icon={back.logo} />
                 </div>
               </div>
